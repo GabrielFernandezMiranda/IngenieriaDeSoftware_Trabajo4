e7c9f2e0351c8d4231630d5950046bf5aa3d68d6
Gabriel Fernandez <gabrielfernandezmiranda@gmail.com>
2026-02-26 00:57:33 -0400

    Feat: Agregando Multiplicador

--- src/presenter.js
+++ src/presenter.js
@@ -1,5 +1,7 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
+// Sumador
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
@@ -12,4 +14,19 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+// Multiplicador
+const firstMult = document.querySelector("#primer-numero-mult");
+const secondMult = document.querySelector("#segundo-numero-mult");
+const formMult = document.querySelector("#multiplicar-form");
+const divMult = document.querySelector("#resultado-mult-div");
+
+formMult.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(firstMult.value);
+  const secondNumber = Number.parseInt(secondMult.value);
+
+  divMult.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
